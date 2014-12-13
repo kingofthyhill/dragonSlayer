e444b23701feffbd3c1671fdbd9f9cbcd2616a2f
Robert Shidner <s3n4t0rl3m0nsn0ut@gmail.com>
2014-12-13 10:57:47 -0900

    full push

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class buttonScript : MonoBehaviour {
+
+	public Button button1;
+
+	// Use this for initialization
+	void Start () {
+		button1 = GetComponent <Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
