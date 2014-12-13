484334337e9db3a19d472dbf423d63872f68009b
Robert Shidner <s3n4t0rl3m0nsn0ut@gmail.com>
2014-12-13 12:56:57 -0900

    Changed the sword from a quad to a button. Now will only trigger when clicked. Edited swordScripts deleted 1 added 1

--- Assets/buttonScript.cs
+++ Assets/buttonScript.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using System.Collections;
 
 public class buttonScript : MonoBehaviour {
 
 	public Button button1;
+	private int count = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +15,10 @@ public class buttonScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//button1.onClick.AddListener (yay)  ;
 	}
+	public void countForMe () {
+		Debug.Log (count);
+		count += 1;
+		}
 }
---
+++ Assets/sword2Script.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class sword2Script : MonoBehaviour {
+
+	//Declare your variables
+	// Create a tracking variable for the damage
+	private int damageCount = 0;
+	// Creates a tracking variable for the damage per hit of the sword
+	private int swordDamage = 1;
+	// Creates an image variable the image.
+	private Image image;
+	// Create five material variables for the different sword types.
+	public Material IronFire, IronFire2, Gold, GoldFire, GoldFire2;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent <Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		levelUp (8, IronFire, false);
+		levelUp (16, IronFire2, false);
+		levelUp (32, Gold, false);
+		levelUp (64, GoldFire, false);
+		levelUp (128, GoldFire2, false);
+	}
+	// When the damageCount gets to damage, it will change the material to swordType
+	void levelUp (int damage, Material swordType, bool triggered){
+		if (damageCount >= damage && !triggered) {
+			image.material = swordType;
+			triggered = true;
+		}
+	}
+	//Increases the damage by swordDamage when it is called
+	public void damage () {
+		damageCount += swordDamage;
+	}
+}
