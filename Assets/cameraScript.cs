9b4727c8b0e6f361d363d36146e3a84fb3a8b46f
Robert Shidner <s3n4t0rl3m0nsn0ut@gmail.com>
2014-12-14 12:30:08 -0900

    Lunch Break Push. Made the shop screen, added more options to that, added health counter, added a new font.

--- Assets/cameraScript.cs
+++ Assets/cameraScript.cs
@@ -3,19 +3,33 @@ using System.Collections;
 
 public class cameraScript : MonoBehaviour {
 
+	private Vector2[]  stages = new Vector2[6]; 
+	private Vector2[] menus = new Vector2[3];
+	private int stage = -1;
+
 	// Use this for initialization
 	void Start () {
-	
+		//Setting the position of the menus
+		menus [0] = new Vector2 (1000, 0);
+		menus [1] = new Vector2 (1000, 350);
+		menus [2] = new Vector2 (1500, 0);
+		//Setting the position of the stages
+		stages[0] = new Vector2 (0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-	public void battleChateau (){
-		transform.position = new Vector3 (0, 0, transform.position.z);
+	public void battle (){
+		if (stage != -1) {
+			transform.position = stages[stage];
+		}
 	}
-	public void stageSelect () {
-		transform.position = new Vector3 (1000, 350, transform.position.z);
+	public void stageSet (int stageSet){
+		stage = stageSet;
+	}
+	public void menu (int menuNum){
+		transform.position = menus [menuNum];
 	}
 }
--- Assets/sword2Script.cs
+++ Assets/sword2Script.cs
@@ -5,38 +5,47 @@ using System.Collections;
 
 public class sword2Script : MonoBehaviour {
 
-	// Declare your variables
-	// Create a tracking variable for the damage and for the experience
+		// Declare your variables
+		// Create a tracking variable for the damage and for the experience
 	private int damageCount = 0, expCount = 0;
-	// Creates a tracking variable for the damage and experience per hit of the sword
+		// Creates a tracking variable for the damage and experience per hit of the sword and sets them both to 1
 	private int swordDamage = 1, swordExperience = 1;
-	// Creates an image variable the image.
+		// Creates an image variable the image.
 	private Image image; 
-	// Creates an interger for the level.
+		// Creates an interger for the level.
 	private int levelGlobal;
-	// Creates a bool array for all the triggers
+		/* Creates a tracking variable for the dragon's health and dragon's health minus damage.
+		Sets the dragons total health to 3000.*/
+	private int dragonHealthTotal = 3000, dragonHealthNum;
+		// Creates a bool array for all the triggers
 	private bool[] trigger = new bool[6];
-	// Create five material variables for the different sword types.
+		// Create five material variables for the different sword types.
 	public Material IronFire, IronFire2, Gold, GoldFire, GoldFire2, BackgroundSkin;
-	// Creates a renderer variable for the background
+		// Creates a renderer variable for the background
 	public Renderer backgroundQuad;
-
-	// Use this for initialization
+		// Creates a text variable for displaying the dragons health
+	public Text healthDisplay;
+	
 	void Start () {
+			// Sets the image component to image.
 		image = GetComponent <Image> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		levelUp (8, IronFire,1,1);
-		levelUp (16, IronFire2,2,2);
-		levelUp (32, Gold, 4,3);
-		levelUp (64, GoldFire,8,4);
-		levelUp (128, GoldFire2,16,5);
-		killingTheDragon (3000, BackgroundSkin, backgroundQuad, 100, 1);
+			// Sets the first five levels of swords
+		levelUp (8, IronFire, 1, 1);
+		levelUp (16, IronFire2, 2, 2);
+		levelUp (32, Gold, 4, 3);
+		levelUp (64, GoldFire, 8, 4);
+		levelUp (128, GoldFire2, 16, 5);
+			// Sets up the events for when the dragon dies.
+		killingTheDragon (dragonHealthTotal , BackgroundSkin, backgroundQuad, 100, 0);
+		dragonHealth (dragonHealthTotal);
+
 	}
-	/* When the expCount gets to the set experience level, it will change the material to swordType, increase the swordDamage with bonusDamage,
-	 and blocks it from happening again.*/
+		/* When the expCount gets to the set experience level, it will change the material to swordType, increase the swordDamage with bonusDamage,
+		 and blocks it from happening again.*/
 	void levelUp (int experience, Material newMaterial,int bonusDamage, int level){
 		if (expCount >= experience && level > levelGlobal) {
 			levelGlobal  += 1;
@@ -45,8 +54,8 @@ public class sword2Script : MonoBehaviour {
 			Debug.Log ("You leveled up!");
 		}
 	}
-	/*Once the damageCount has reached the set damage level, it will change the background to backgroundSkin, give you bonusExp, and 
-	 blocks it from happening again*/
+		/*Once the damageCount has reached the set damage level, it will change the background to backgroundSkin, give you bonusExp, and 
+		 blocks it from happening again*/
 	void killingTheDragon (int damage, Material backgroundSkin, Renderer background, int bonusExp, int triggerNum) {
 		if (damageCount >= damage && !trigger[triggerNum]) {
 			background.material = backgroundSkin;
@@ -54,9 +63,26 @@ public class sword2Script : MonoBehaviour {
 			trigger[triggerNum] = true;
 		}
 	}
-	//Increases the damage by swordDamage amd exp by swordExperience when it is called
+		// Displays the dragon's health as a changing variable.
+	void dragonHealth(int maxHealth) {
+		dragonHealthNum = maxHealth - damageCount;
+		if (dragonHealthNum > 0) {
+			healthDisplay.text = "Health: " + dragonHealthNum.ToString ();
+		} else if (dragonHealthNum <= 0) {
+			healthDisplay.text = "Health: Dead";
+		}
+	}
+		//Increases the damage by swordDamage amd exp by swordExperience when it is called
 	public void attack () {
 		damageCount += swordDamage;
 		expCount += swordExperience;
+	}
+		// Allows for increases of swordDamage.
+	public void bonusDPH (int bonus) {
+			swordDamage += bonus;
+	}
+		// Allows for increases of swordExperience
+	public void bonusEPH (int bonus) {
+		swordExperience += bonus;
 	}
 }
