cb7507543959d015275f9b386090bbbfe5397342
Robert Shidner <s3n4t0rl3m0nsn0ut@gmail.com>
2014-12-14 15:59:19 -0900

    End of Day Push 12/14/14; Split sword2script into two different scripts. Still needs some jiggering to be fixed.

---
+++ Assets/dragonScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class dragonScript : MonoBehaviour {
+
+	// Creates a tracking variable for the dragon's health minus damage.
+	private int dragonHealthNum;
+	// Creates a bool array for all the triggers
+	private bool[] trigger = new bool[6];
+	public Material backgroundSkin;
+	// Creates a renderer variable for the background
+	public Renderer backgroundQuad;
+	// Creates a text variable for displaying the dragons health
+	public Text healthDisplay;
+	// Creates a modifiable variable for the dragon's health
+	public int dragonHealthTotal;
+	public playerScript player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Sets up the events for when the dragon dies.
+		killingTheDragon (dragonHealthTotal,backgroundSkin, backgroundQuad, 100, 0);
+		dragonHealth (dragonHealthTotal);
+	}
+	/*Once the damageCount has reached the set damage level, it will change the background to backgroundSkin, give you bonusExp, and 
+		 blocks it from happening again*/
+	void killingTheDragon (int damage, Material backgroundSkin, Renderer background, int bonusExp, int triggerNum) {
+		if (player.damageCount >= damage && !trigger[triggerNum]) {
+			background.material = backgroundSkin;
+			player.expCount += bonusExp;
+			trigger[triggerNum] = true;
+		}
+	}
+	// Displays the dragon's health as a changing variable.
+	void dragonHealth(int maxHealth) {
+		dragonHealthNum = maxHealth - player.damageCount;
+		if (dragonHealthNum > 0) {
+			healthDisplay.text = "Health: " + dragonHealthNum.ToString ();
+		} else if (dragonHealthNum <= 0) {
+			healthDisplay.text = "Health: Dead";
+		}
+	}
+}
---
+++ Assets/playerScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+	
+	// Declare your variables
+	// Create a tracking variable for the gold and for the experience
+	private int goldCount;
+	//Creates a tracking variable for the gold timer.
+	private int goldTimer;
+	// Creates a tracking variable for the damage and experience per hit of the sword and sets them both to 1
+	private int swordDamage = 1, swordExperience = 1; 
+	// Creates an interger for the level.
+	private int levelGlobal;
+	// Create five material variables for the different sword types.
+	public Material[] swordType = new Material[5];
+	// Creates an image variable the image.
+	public Image[] swords = new Image[2];
+	// Creates a refrenceable variable for the damage and exp
+	public int expCount, damageCount;
+	
+	// Update is called once per frame
+	void Update () {
+		// Sets the first five levels of swords
+		levelUp (8, 0, 1, 1);
+		levelUp (16, 1, 2, 2);
+		levelUp (32, 2, 4, 3);
+		levelUp (64, 3, 8, 4);
+		levelUp (128, 4, 16, 5);
+		
+	}
+	/* When the expCount gets to the set experience level, it will change the material to swordType, increase the swordDamage with bonusDamage,
+		 and blocks it from happening again.*/
+	void levelUp (int experience, int materialNum ,int bonusDamage, int level){
+		if (expCount >= experience && level > levelGlobal) {
+			levelGlobal  += 1;
+			for (int i=0; i < swords.Length; i++) {
+				swords[i].material = swordType[materialNum];
+			}
+			swordDamage += bonusDamage;
+			Debug.Log ("You leveled up!");
+		}
+	}
+	//Increases the damage by swordDamage amd exp by swordExperience when it is called
+	public void attack () {
+		damageCount += swordDamage;
+		expCount += swordExperience;
+		if (goldTimer == 5) {
+			goldCount += 1;
+		} else {
+			goldTimer += 1;
+		}
+	}
+	// Allows for increases of swordDamage.
+	public void bonusDPH (int bonus) {
+		swordDamage += bonus;
+	}
+	// Allows for increases of swordExperience
+	public void bonusEPH (int bonus) {
+		swordExperience += bonus;
+	}
+}
